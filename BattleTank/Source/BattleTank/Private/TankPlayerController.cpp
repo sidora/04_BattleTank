dcdcd2376c9d23727b68b9577a8731ee5369710f
Yan Doiron <yandoiron@hotmail.com>
2016-06-29 16:45:49 -0400

    added c++ class for pawn and PController

---
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BattleTank.h"
+#include "TankPlayerController.h"
+
+void ATankPlayerController::BeginPlay() 
+{
+	Super::BeginPlay();
+
+	auto ControlledTank = GetControlledTank();
+
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController not possessing a "));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController possessing %s"), *(ControlledTank->GetName()));
+	}
+}
+
+
+ATank* ATankPlayerController::GetControlledTank() const 
+{
+	return Cast<ATank>(GetPawn());
+
+
+}
